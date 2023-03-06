1b5bf72bf3cd0aca33a8479920613e1330cf57f0
JustGeniuss <danila.kukushkin98@gmail.com>
2023-03-06 10:23:24 +0300

    add typescript, Logger, baseController and Routing

---
+++ src/users/user.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+
+const userRouter = express.Router();
+
+
+userRouter.use((req, res, next) => {
+    console.log('Users Handlers');
+    next();
+})
+
+
+userRouter.post('/login', (req, res) => {
+    res.send('login');
+})
+
+
+
+userRouter.post('/register', (req, res) => {
+    res.send('register');
+})
+
+
+export { userRouter };
