3c42a5c367bedd8e75ca5ed078126280233e74ea
JustGeniuss <danila.kukushkin98@gmail.com>
2023-03-16 20:28:46 +0300

    add jwt to userLogin

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -2,20 +2,23 @@ import { NextFunction, Response, Request } from 'express';
 import { inject, injectable } from 'inversify';
 import { BaseController } from '../common/base.controller';
 import { HttpError } from '../errors/http-error.class';
-import { LoggerService } from '../logger/logger.service';
 import { TYPES } from '../types';
 import 'reflect-metadata';
 import { IUserController } from './user.controller.interface';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
-import { UserService } from './users.service';
 import { ValidateMiddleware } from '../common/validate.middleware';
+import { sign } from 'jsonwebtoken';
+import { IConfigService } from '../config/config.service.interface';
+import { ILogger } from '../logger/logger.interface';
+import { IUserService } from './users.service.interface';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
 	constructor(
-		@inject(TYPES.ILogger) private loggerService: LoggerService,
-		@inject(TYPES.UserService) private userService: UserService,
+		@inject(TYPES.ILogger) private loggerService: ILogger,
+		@inject(TYPES.UserService) private userService: IUserService,
+		@inject(TYPES.ConfigService) private configService: IConfigService,
 	) {
 		super(loggerService);
 		this.bindRoutes([
@@ -43,7 +46,8 @@ export class UserController extends BaseController implements IUserController {
 		if (!user) {
 			return next(new HttpError(401, 'Ошибка авторизация', 'login'));
 		}
-		this.ok(res, { message: 'Вы успешно авторизовались' });
+		const jwt = await this.signJwt(body.email, this.configService.get('SECRET'));
+		this.ok(res, { jwt });
 	}
 
 	async register(
@@ -56,5 +60,24 @@ export class UserController extends BaseController implements IUserController {
 			return next(new HttpError(422, 'Такой пользователь уже существует'));
 		}
 		this.ok(res, { email: result.email, id: result.id });
+	}
+
+	private signJwt(email: string, secret: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
+			sign(
+				{
+					email,
+					iat: Math.floor(Date.now() / 1000),
+				},
+				secret,
+				{ algorithm: 'HS256' },
+				(err, token) => {
+					if (err) {
+						reject(err);
+					}
+					resolve(token as string);
+				},
+			);
+		});
 	}
 }
